e4c1aad4913ec067a16577bb5854dfa2befaa733
Silas Franca <silas.franca@fatec.sp.gov.br>
2022-11-03 06:07:49 -0300

    Create LT01EX09.py

---
+++ LT01EX09.py
@@ -0,0 +1,4 @@
+num1 = float(input("Informe o primeiro número "))
+num2 = float(input("informe o segundo número "))
+calc = (num1 ** 2 ) + (num2 ** 2 )
+print(" O valor da soma dos quadrados dos números é ", calc)

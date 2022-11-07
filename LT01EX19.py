83a70def13d20057836ef56c82a510982c8b0906
Silas Franca <silas.franca@fatec.sp.gov.br>
2022-11-07 06:22:33 -0300

    Create LT01EX19.py

---
+++ LT01EX19.py
@@ -0,0 +1,9 @@
+n1 = int(input('Informe o primeiro número '))
+n2 = int(input('Informe o Segundo número '))
+
+if n1 > n2:
+    print('O número maior entre eles é o', n1)
+elif n2 > n1:
+    print('O número maior entre eles é o', n2)
+else:
+    print('Os números são iguais')
